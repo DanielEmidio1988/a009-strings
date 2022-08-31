9efeee335bfebcc1a9ba2967e1ac67e37d153977
DanielEmidio1988 <daniel.emidio.silva@gmail.com>
2022-08-31 15:39:49 -0300

    Entregando o exercicio da aula de hoje (31/08)

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,28 @@
-//Crie a const para a frase aqui
+// # Exercício 3
+
+
+// A partir da frase a seguir, faça o que se pede
+
+// ```
+// Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"
+// ```
+
+// a) Crie uma `const` no seu código para guardar a frase (com aspas e tudo);
+let frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\""
+
+
+// b) Crie uma nova string a partir da primeira, trocando **verde** por **rosa**, e **azul** por **laranja**;
+
+const novaFrase = frase.replace("verde","rosa").replace("azul","laranja")
+
+console.log(novaFrase)
+
+// c) Verifique se a nova string inclui **verde**, e se inclui **laranja**.
+//Ok
+
+
+// **EXTRA:** tente fazer o “mas não deixe o gato sair” ficar em maiúsculo, assim como o “BOAS VINDAS”
+
+frase = frase.replace("mas não deixe o gato sair","MAS NÃO DEIXE O GATO SAIR")
+console.log(frase)
+
